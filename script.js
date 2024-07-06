8af293e9f7b9bfae18f2b1de1da0296ef4717f08
KingAbby200 <139162103+KingAbby200@users.noreply.github.com>
2024-07-06 20:43:00 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function openNav() {
+    document.getElementById("sidebar").style.width = "400px";
+}
+
+function closeNav() {
+    document.getElementById("sidebar").style.width = "0px";
+}
+
+function goals() {
+    window.location = "#goals";
+}
+function achievements() {
+    window.location = "#achievements";
+}
+
